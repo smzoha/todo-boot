d1d15c17347a5b9fc45bb94dd1075906caef07fb
Shamah M Zoha <shamah1992@gmail.com>
2021-05-04 21:20:13 +0600

    Added in-memory test user for testing
    This creates a test user with a defined password, which is only used for
    testing out the login page. This will soon be replaced with a more
    standard approach.

--- src/main/java/com/zedapps/todoboot/config/SecurityConfig.java
+++ src/main/java/com/zedapps/todoboot/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.zedapps.todoboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Shamah M Zoha
@@ -25,5 +28,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/", true);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("test-user")
+                .password(passwordEncoder().encode("testpass"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
