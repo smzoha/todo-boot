2a51580437088f57ba995ba7d5a181c1b91cb782
Shamah M Zoha <shamah1992@gmail.com>
2021-05-11 22:51:57 +0600

    Added JDBC based authentication
    Replacing the in-memory test user, JDBC based authentication is used for
    the application moving forward, including some customization to account
    for the table names.
    
    The datasource defined in the properties file is used, as well as the
    BCrypt Password Encoder that's defined as the bean in the configuration
    file.
    
    By default, Spring Security expects users and authorities tables, but if
    these names are different in the project, additional method calls are
    required to provide the SQL needed to pull the authentication details.
    These SQLs, based on the application's database design, is added to the
    config file.

--- src/main/java/com/zedapps/todoboot/config/SecurityConfig.java
+++ src/main/java/com/zedapps/todoboot/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zedapps.todoboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 /**
  * @author Shamah M Zoha
  * @since 5/4/2021
@@ -16,6 +19,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String USER_BY_USERNAME_SQL = "SELECT username, password, active" +
+            " FROM user_account" +
+            " WHERE username = ?";
+
+    private static final String PRIVILEGE_BY_USERNAME_SQL = "SELECT username, privilege" +
+            " FROM privilege" +
+            " WHERE username = ?";
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,10 +46,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("test-user")
-                .password(passwordEncoder().encode("testpass"))
-                .roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery(USER_BY_USERNAME_SQL)
+                .authoritiesByUsernameQuery(PRIVILEGE_BY_USERNAME_SQL);
     }
 
     @Bean
