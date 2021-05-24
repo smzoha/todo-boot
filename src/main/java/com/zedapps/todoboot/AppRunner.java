03c6a6058769a4dc2e5296257aca6a2a6e81bf51
Shamah M Zoha <shamah1992@gmail.com>
2021-05-25 00:42:07 +0600

    Added message resource-based validation beans
    Added MessageSource & LocalValidatorFactoryBean to AppRunner class, in
    order to have messages.properties file in resources folder, and use it
    in validations and custom messages in jsp views.

--- src/main/java/com/zedapps/todoboot/AppRunner.java
+++ src/main/java/com/zedapps/todoboot/AppRunner.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
  * @author Shamah M Zoha
@@ -19,5 +23,22 @@ public class AppRunner extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppRunner.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+        validatorFactoryBean.setValidationMessageSource(messageSource());
+
+        return validatorFactoryBean;
     }
 }
