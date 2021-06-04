9d5324dbecc59b63c57699afa01efa40e36e144c
Shamah M Zoha <shamah1992@gmail.com>
2021-06-04 19:58:21 +0600

    Refactored Task creation flow to use current user
    Previously, in order to test task creation, a static user id was used to
    create the tasks. This has since been refactored to use the user that's
    currently logged into the application.

--- src/main/java/com/zedapps/todoboot/controller/TaskController.java
+++ src/main/java/com/zedapps/todoboot/controller/TaskController.java
@@ -1,7 +1,7 @@
 package com.zedapps.todoboot.controller;
 
+import com.zedapps.todoboot.config.AppUserDetails;
 import com.zedapps.todoboot.entity.Task;
-import com.zedapps.todoboot.entity.User;
 import com.zedapps.todoboot.entity.enums.Priority;
 import com.zedapps.todoboot.entity.enums.Status;
 import com.zedapps.todoboot.repository.TaskRepository;
@@ -9,6 +9,8 @@ import com.zedapps.todoboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
@@ -45,10 +47,8 @@ public class TaskController {
     }
 
     @GetMapping
-    public String getTask(@RequestParam(required = false) Long taskId,
-                          ModelMap model) {
-
-        Task task = getTask(userRepository.findById(1L).orElse(null), taskId);
+    public String getTask(@RequestParam(required = false) Long taskId, Authentication auth, ModelMap model) {
+        Task task = getTask(taskId, ((AppUserDetails) auth.getPrincipal()).getUserId());
 
         model.put("task", task);
         model.put("priorityList", Arrays.asList(Priority.values()));
@@ -79,9 +79,9 @@ public class TaskController {
         return "redirect:task";
     }
 
-    private Task getTask(User currentUser, Long taskId) {
+    private Task getTask(Long taskId, long userId) {
         if (Objects.isNull(taskId)) {
-            return new Task(currentUser);
+            return new Task(userRepository.findById(userId).orElse(null));
         }
 
         Task task = taskRepository.findById(taskId).orElse(null);
